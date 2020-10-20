65c3298077c5e1c374edd18a722712955077ac77
Shmuel <samuel.sahala@ricult.com>
2020-10-20 13:21:57 +0300

    move middlewares

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan'); // logger
 const helmet = require('helmet'); // secure headers
 const cors = require('cors');
+const middlewares = require('./middlewares');
 
 const app = express();
 app.use(morgan('common'));
@@ -15,23 +16,8 @@ app.get('/', (req, res) => {
 
 });
 
-// -- Middlewares
-// not found handler
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl} `);
-  res.status(404);
-  next(error);
-});
-// error handler
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? 'shmuel' : error.stack, // show stack tail
-  });
-});
-
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
