d156aa2742f1dcf06443ee6670af8fe113cccab7
Shmuel <samuel.sahala@ricult.com>
2020-10-20 13:17:51 +0300

    add midlewares error handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 const express = require('express');
+const morgan = require('morgan'); // logger
+const helmet = require('helmet'); // secure headers
+const cors = require('cors');
 
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+// -- Routes
+app.get('/', (req, res) => {
+
+});
+
+// -- Middlewares
+// not found handler
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl} `);
+  res.status(404);
+  next(error);
+});
+// error handler
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'shmuel' : error.stack, // show stack tail
+  });
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
